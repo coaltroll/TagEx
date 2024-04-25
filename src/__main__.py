3b074d78e349b1b88f8ae7976399011abc93bbd4
coaltroll <coaltroll@users.noreply.github.com>
2024-04-25 13:27:33 +0000

    Formatted files with ruff through Github Actions.

--- src/__main__.py
+++ src/__main__.py
@@ -223,25 +223,28 @@
 import wx
 import os
 
+
 class MainWindow(wx.Frame):
     def __init__(self, parent, title):
-        self.dirname=''
+        self.dirname = ""
 
         # A "-1" in the size parameter instructs wxWidgets to use the default size.
         # In this case, we select 200px width and the default height.
-        wx.Frame.__init__(self, parent, title=title, size=(200,-1))
+        wx.Frame.__init__(self, parent, title=title, size=(200, -1))
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.CreateStatusBar() # A Statusbar in the bottom of the window
+        self.CreateStatusBar()  # A Statusbar in the bottom of the window
 
         # Setting up the menu.
-        filemenu= wx.Menu()
-        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open"," Open a file to edit")
-        menuAbout= filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-        menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+        filemenu = wx.Menu()
+        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open", " Open a file to edit")
+        menuAbout = filemenu.Append(
+            wx.ID_ABOUT, "&About", " Information about this program"
+        )
+        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
 
         # Creating the menubar.
         menuBar = wx.MenuBar()
-        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+        menuBar.Append(filemenu, "&File")  # Adding the "filemenu" to the MenuBar
         self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
 
         # Events.
@@ -252,7 +255,7 @@ class MainWindow(wx.Frame):
         self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
         self.buttons = []
         for i in range(0, 6):
-            self.buttons.append(wx.Button(self, -1, "Button &"+str(i)))
+            self.buttons.append(wx.Button(self, -1, "Button &" + str(i)))
             self.sizer2.Add(self.buttons[i], 1, wx.EXPAND)
 
         # Use some sizers to see layout options
@@ -260,7 +263,7 @@ class MainWindow(wx.Frame):
         self.sizer.Add(self.control, 1, wx.EXPAND)
         self.sizer.Add(self.sizer2, 0, wx.EXPAND)
 
-        #Layout sizers
+        # Layout sizers
         self.SetSizer(self.sizer)
         self.SetAutoLayout(1)
         self.sizer.Fit(self)
@@ -268,7 +271,11 @@ class MainWindow(wx.Frame):
 
     def OnAbout(self, e):
         # A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets.
-        dlg = wx.MessageDialog(self, "TagEx is a CLI tool to manually modify music file metadata efficiently and conveniently.", "About TagEx")
+        dlg = wx.MessageDialog(
+            self,
+            "TagEx is a CLI tool to manually modify music file metadata efficiently and conveniently.",
+            "About TagEx",
+        )
         dlg.ShowModal()  # Show it
         dlg.Destroy()  # finally destroy it when finished.
 
@@ -280,10 +287,14 @@ class MainWindow(wx.Frame):
         if dlg.ShowModal() == wx.ID_OK:
             self.filename = dlg.GetFilename()
             self.dirname = dlg.GetDirectory()
-            f = open(os.path.normpath(os.path.join(self.dirname+os.sep,self.filename)),'r')
+            f = open(
+                os.path.normpath(os.path.join(self.dirname + os.sep, self.filename)),
+                "r",
+            )
             self.control.SetValue(f.read())
             f.close()
         dlg.Destroy()
+
 
 app = wx.App(False)
 frame = MainWindow(None, "TagEx")
