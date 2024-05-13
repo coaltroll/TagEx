fd3124af14369978ac1debb66f3190aac082b293
coaltroll <cg.co.gu@gmail.com>
2024-05-13 09:58:29 +0200

    changed sperm emoticon to happy flower face emoticon

--- reset_samples.sh
+++ reset_samples.sh
@@ -6,7 +6,7 @@ fi
 
 if [ -d "samples_original" ]; then
     cp -r "samples_original" "samples_testing"
-    echo "OK! (✿◠‿◠)"
+    echo "OK! (✿ ◠ ‿ ◠)"
 else
     echo "Error: 'samples_original' directory does not exist."
     exit 1
